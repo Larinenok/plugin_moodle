945a8900f76ec98c71720a0519181ade8579a788
Larinenok <60542488+Larinenok@users.noreply.github.com>
2024-10-16 22:50:48 +0700

    Update uninstall.php

--- db/uninstall.php
+++ db/uninstall.php
@@ -1,12 +1,10 @@
 <?php
 
-// Убедитесь, что файл подключен через Moodle API.
 defined('MOODLE_INTERNAL') || die();
 
 function xmldb_block_calculator_uninstall() {
     global $DB;
 
-    // Удалить все данные из таблиц, связанных с плагином.
     $DB->delete_records('calculator_history');
     
     return true;
